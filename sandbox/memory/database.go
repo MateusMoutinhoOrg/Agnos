093e5121e46dee9281fdca548cda99b26aa74752
matesmoutinho <matesmoutinho01@gmail.com>
2026-08-25 06:15:10 -0300

    refactor: replace user schema with config and states schemas in database definition

--- sandbox/memory/database.go
+++ sandbox/memory/database.go
@@ -4,22 +4,20 @@ import (
 	"github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/contracts/deps/keepdeps"
 )
 
-var Schemas = []keeptypes.Schema{
+var Schemas = []keepdeps.Schema{
 	{
-		Name: "user",
-		Itens: []keeptypes.Item{
-			{Name: "email", Type: keeptypes.Key, Required: true},
-			{Name: "username", Type: keeptypes.Key, Required: true},
-			{Name: "age", Type: keeptypes.Int, Required: true},
-			{
-				Name: "sessions",
-				Type: keeptypes.Database,
-				Itens: []keeptypes.Item{
-					{Name: "token", Type: keeptypes.Key, Required: true},
-					{Name: "creation", Type: keeptypes.Int, Required: true},
-					{Name: "expiration", Type: keeptypes.Int, Required: true},
-				},
-			},
+		Name: "config",
+		Itens: []keepdeps.Item{
+			{Name: "name", Type: keepdeps.Key, Required: true},
+			{Name: "value", Type: keepdeps, Required: true},
+
+		},
+	},
+	{
+		Name: "states",
+		Itens: []keepdeps.Item{
+			{Name: "name", Type: keepdeps.Key, Required: true},
+			{}
 		},
 	},
 }
