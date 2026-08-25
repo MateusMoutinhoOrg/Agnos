0776f1efd7c00069c4577937b9342227ea9b3fd9
matesmoutinho <matesmoutinho01@gmail.com>
2026-08-25 03:24:08 -0300

    feat: define Memory struct and implement factory pattern for memory initialization

--- sandbox/contracts/api/memory.go
+++ sandbox/contracts/api/memory.go
@@ -1,4 +1,8 @@
 package api
 
+type Memory struct {
+}
+
 type MemoryApi struct {
+	NewMemory func(path string) Memory
 }
---
+++ sandbox/memory/new.go
@@ -0,0 +1,12 @@
+package memory
+
+import (
+	"log"
+
+	"github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/contracts/api"
+	"github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/contracts/deps/keepdeps"
+)
+
+func NewMemoryFactory(api path string) api.Memory {
+	
+}
