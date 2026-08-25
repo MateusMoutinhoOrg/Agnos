dfdaac0bb2834f5873ff9b9190b53b978d16e5b4
matesmoutinho <matesmoutinho01@gmail.com>
2026-08-25 05:41:11 -0300

    feat: add database dependency and state management functions to Memory struct

--- sandbox/contracts/api/memory.go
+++ sandbox/contracts/api/memory.go
@@ -1,6 +1,17 @@
 package api
 
+import (
+	"github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/contracts/deps/keepdeps"
+)
+
 type Memory struct {
+	Database keepdeps.KeepDatabase
+
+	GetProjectName func() string
+	GetVersion     func() string
+	IsStateActive  func(state string) bool
+	ActiveState    func(state string)
+	DeactiveState  func(state string)
 }
 
 type MemoryApi struct {
