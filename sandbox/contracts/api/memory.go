c41b3ea642cc7cbfb996cf78c283f07641913fbf
matesmoutinho <matesmoutinho01@gmail.com>
2026-08-25 06:51:42 -0300

    feat: define CLI structure types and update Memory interface to support command-line definitions

--- sandbox/contracts/api/memory.go
+++ sandbox/contracts/api/memory.go
@@ -1,7 +1,41 @@
 package api
 
+type MemoryCliFlag struct {
+	Id               string
+	ValidIdentifiers []string
+
+	Description string
+	Examples    []string
+
+	Type             int
+	RequiredMinSize  int
+	RequiredMaxSize  int
+	RequiredPresence bool
+}
+
+type MemoryCliArg struct {
+	Id           string
+	Description  string
+	Examples     []string
+	RequiredType int
+	RequiredSize int
+}
+
+type MemoryCliCommand struct {
+	ValidStartIdentifiers []string
+	Args                  []MemoryCliArg
+	Flags                 []MemoryCliFlag
+	Description           string
+	Examples              []string
+}
+
 type Memory struct {
-	GetProjectName func() string
+	Path        string
+	ProjectName string
+	Version     func() string
+	Commands    []MemoryCliCommand
+
+	Persist func()
 }
 
 type MemoryApi struct {
