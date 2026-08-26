e01e58d1ac88790875602e0c073778eb289ec84d
matesmoutinho <matesmoutinho01@gmail.com>
2026-08-26 03:04:43 -0300

    feat: add force flag to StartFactory and support optional module initialization

--- sandbox/core/StartFactory.go
+++ sandbox/core/StartFactory.go
@@ -5,13 +5,15 @@ import (
 	"github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/userconfig"
 )
 
-func StartFactory(sandbox *api.SandBox) func(path string, project_name string, module string) error {
+func StartFactory(sandbox *api.SandBox) func(path string, project_name string, module *string, force bool) error {
 
-	return func(path string, project_name string, module string) error {
+	return func(path string, project_name string, module *string, force bool) error {
 
 		configDir := path + "/" + sandbox.ProjectName + "Config"
-		if sandbox.Deps.IoLib.IsDir(configDir) {
-			return sandbox.Deps.Errorf("the %s folder already exists", sandbox.ProjectName)
+		if !force {
+			if sandbox.Deps.IoLib.IsDir(configDir) {
+				return sandbox.Deps.Errorf("the %s folder already exists", sandbox.ProjectName)
+			}
 		}
 
 		project_specs_dest := configDir + "/project.yaml"
@@ -22,6 +24,26 @@ func StartFactory(sandbox *api.SandBox) func(path string, project_name string, m
 
 		project_conf.Name = project_name
 		project_conf.Description = "Place the Project Description in " + project_specs_dest
+
+		if module != nil {
+
+			module_path := path + "go.mod"
+
+			if !force {
+				if sandbox.Deps.IoLib.IsFile(module_path) {
+					return sandbox.Deps.Errorf("the %s file already exists", "go.mod")
+				}
+			}
+			module_conf, err := userconfig.NewModuleConf(sandbox, module_path)
+			if err != nil {
+				return err
+			}
+			module_conf.Module = *module
+			err = module_conf.Persist()
+			if err != nil {
+				return err
+			}
+		}
 
 		err = project_conf.Persist()
 		if err != nil {
