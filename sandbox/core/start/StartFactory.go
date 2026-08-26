0830d02c21010f0d773c8aab33b900b87ed4c932
matesmoutinho <matesmoutinho01@gmail.com>
2026-08-26 06:20:30 -0300

    refactor: extract project configuration logic into createProjectSpecs function

--- sandbox/core/start/StartFactory.go
+++ sandbox/core/start/StartFactory.go
@@ -5,6 +5,44 @@ import (
 	"github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/userconfig"
 )
 
+func createProjectSpecs(sandbox *api.SandBox, props api.StartProps, configDir string) error {
+
+	project_specs_dest := configDir + "/project.yaml"
+	project_conf, err := userconfig.NewProjectConf(sandbox, project_specs_dest)
+	if err != nil {
+		return err
+	}
+
+	project_conf.Name = props.ProjectName
+	project_conf.Description = "Place the Project Description in " + project_specs_dest
+
+	if props.Module != nil {
+
+		module_path := props.Path + "/go.mod"
+
+		if !props.Force {
+			if sandbox.Deps.IoLib.IsFile(module_path) {
+				return sandbox.Deps.Errorf("the %s file already exists", "go.mod")
+			}
+		}
+		module_conf, err := userconfig.NewModuleConf(sandbox, module_path)
+		if err != nil {
+			return err
+		}
+		module_conf.Module = *props.Module
+		err = module_conf.Persist()
+		if err != nil {
+			return err
+		}
+	}
+
+	err = project_conf.Persist()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func StartFactory(sandbox *api.SandBox) func(props api.StartProps) error {
 
 	return func(props api.StartProps) error {
@@ -15,37 +53,7 @@ func StartFactory(sandbox *api.SandBox) func(props api.StartProps) error {
 				return sandbox.Deps.Errorf("the %s folder already exists", sandbox.ProjectName)
 			}
 		}
-
-		project_specs_dest := configDir + "/project.yaml"
-		project_conf, err := userconfig.NewProjectConf(sandbox, project_specs_dest)
-		if err != nil {
-			return err
-		}
-
-		project_conf.Name = props.ProjectName
-		project_conf.Description = "Place the Project Description in " + project_specs_dest
-
-		if props.Module != nil {
-
-			module_path := props.Path + "/go.mod"
-
-			if !props.Force {
-				if sandbox.Deps.IoLib.IsFile(module_path) {
-					return sandbox.Deps.Errorf("the %s file already exists", "go.mod")
-				}
-			}
-			module_conf, err := userconfig.NewModuleConf(sandbox, module_path)
-			if err != nil {
-				return err
-			}
-			module_conf.Module = *props.Module
-			err = module_conf.Persist()
-			if err != nil {
-				return err
-			}
-		}
-
-		err = project_conf.Persist()
+		err := createProjectSpecs(sandbox, props, configDir)
 		if err != nil {
 			return err
 		}
