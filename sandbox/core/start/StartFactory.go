a522a20de400328a123e692e41b5ce41a25e63e5
matesmoutinho <matesmoutinho01@gmail.com>
2026-08-27 18:43:47 -0300

    refactor: remove project specification and theme initialization logic from project startup process

--- sandbox/core/start/StartFactory.go
+++ sandbox/core/start/StartFactory.go
@@ -9,20 +9,6 @@ func StartFactory(sandbox *api.SandBox) func(props api.StartProps) error {
 	return func(props api.StartProps) error {
 
 		configDir := props.Path + "/" + sandbox.ProjectName + "Config"
-		if !props.Force {
-			if sandbox.Deps.IoLib.IsDir(configDir) {
-				return sandbox.Deps.Errorf("the %s folder already exists", sandbox.ProjectName)
-			}
-		}
-		err := createProjectSpecs(sandbox, props, configDir)
-		if err != nil {
-			return err
-		}
-
-		err = createThemes(sandbox, props, configDir)
-		if err != nil {
-			return err
-		}
 
 		sandbox.Deps.Printf("started with path %s \n", props.Path)
 		return nil
