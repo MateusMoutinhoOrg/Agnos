b29c0c5f78f06115c1e1c6e9a3f5c7f9c7f0f541
matesmoutinho <matesmoutinho01@gmail.com>
2026-08-27 18:43:56 -0300

    refactor: comment out unused config directory path generation in StartFactory

--- sandbox/core/start/StartFactory.go
+++ sandbox/core/start/StartFactory.go
@@ -8,7 +8,7 @@ func StartFactory(sandbox *api.SandBox) func(props api.StartProps) error {
 
 	return func(props api.StartProps) error {
 
-		configDir := props.Path + "/" + sandbox.ProjectName + "Config"
+		//configDir := props.Path + "/" + sandbox.ProjectName + "Config"
 
 		sandbox.Deps.Printf("started with path %s \n", props.Path)
 		return nil
