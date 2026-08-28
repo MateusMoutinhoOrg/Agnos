f98c334f6e2a57e86e0d4998d87c4ef63411ef2b
matesmoutinho <matesmoutinho01@gmail.com>
2026-08-27 21:40:44 -0300

    chore: bump sandbox version to v0.0.12

--- sandbox/Config.go
+++ sandbox/Config.go
@@ -3,7 +3,7 @@ package sandbox
 import "github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/contracts/api"
 
 func Config(sandbox *api.SandBox) {
-	sandbox.Version = "v0.0.11"
+	sandbox.Version = "v0.0.12"
 	sandbox.ProjectName = "Agnos"
 
 }
