37792287c62305e304c0ffa4b7caa6649d1221bd
matesmoutinho <matesmoutinho01@gmail.com>
2026-08-27 21:40:22 -0300

    feat: implement project scaffolding with atomic file writing using SmartIO

--- sandbox/core/start/StartFactory.go
+++ sandbox/core/start/StartFactory.go
@@ -2,13 +2,70 @@ package start
 
 import (
 	"github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/contracts/api"
+	"github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/utils/parsables"
+	"github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/utils/smartio"
 )
 
+// goVersion is the go directive written into a generated go.mod.
+const goVersion = "1.25.0"
+
+// StartFactory returns the closure that fills api.CoreApi.Start, scaffolding the
+// project config directory with one empty yaml per parsable and, when a module
+// is given, a go.mod at the project root. Every write goes through a SmartIO
+// transaction, so nothing touches the disk unless all of them succeed.
 func StartFactory(sandbox *api.SandBox) func(props api.StartProps) error {
 
 	return func(props api.StartProps) error {
 
-		//configDir := props.Path + "/" + sandbox.ProjectName + "Config"
+		io := smartio.NewSmartIO(sandbox, props.Path)
+
+		write := io.WriteFile
+		if props.Force {
+			write = io.WriteFileOverwrite
+		}
+
+		configDir := props.Path + "/" + sandbox.ProjectName + "Config"
+		io.CreateDir(configDir)
+
+		project_conf := parsables.NewProjectConfEmpty(sandbox)
+		project_conf.Name = props.ProjectName
+
+		themes_conf := parsables.NewThemesConfEmpty(sandbox)
+		ignorable_conf := parsables.NewIgnorableConfEmpty(sandbox)
+		path_replacer_conf := parsables.NewPathReplacerConfEmpty(sandbox)
+
+		parsable_files := []struct {
+			path    string
+			content string
+		}{
+			{configDir + "/project.yaml", project_conf.Render()},
+			{configDir + "/themes.yaml", themes_conf.Render()},
+			{configDir + "/ignore.yaml", ignorable_conf.Render()},
+			{configDir + "/paths.yaml", path_replacer_conf.Render()},
+		}
+
+		for _, file := range parsable_files {
+			write_error := write(file.path, []byte(file.content))
+			if write_error != nil {
+				return write_error
+			}
+		}
+
+		if props.Module != nil {
+			module_conf := parsables.NewModuleConfEmpty(sandbox)
+			module_conf.Module = *props.Module
+			module_conf.GoVersion = goVersion
+
+			write_error := write(props.Path+"/go.mod", []byte(module_conf.Render()))
+			if write_error != nil {
+				return write_error
+			}
+		}
+
+		persist_error := io.Persist()
+		if persist_error != nil {
+			return persist_error
+		}
 
 		sandbox.Deps.Printf("started with path %s \n", props.Path)
 		return nil
