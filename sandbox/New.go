a009c5be51332e82dde8cdaf6f64fdb55ed42708
matesmoutinho <matesmoutinho01@gmail.com>
2026-08-29 03:37:36 -0300

    refactor: update CLI command imports, types, and pass sandbox by reference to factories

--- sandbox/New.go
+++ sandbox/New.go
@@ -1,10 +1,17 @@
 package sandbox
 
 import (
-	"github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/cli"
-	"github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/cli/commands/help"
-	"github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/cli/commands/version"
+	cliMain "github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/cli"
+	cmdBuild "github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/cli/commands/build"
+	cmdExtensionhelp "github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/cli/commands/extensionhelp"
+	cmdHelp "github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/cli/commands/help"
+	cmdInstall "github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/cli/commands/install"
+	cmdList "github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/cli/commands/list"
+	cmdStart "github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/cli/commands/start"
+	cmdUninstall "github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/cli/commands/uninstall"
+	cmdVersion "github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/cli/commands/version"
 	"github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/contracts/deps"
+	cliApi "github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/contracts/lib/cli"
 	"github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/contracts/lib/sandbox"
 	"github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/core/build"
 	"github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/core/extensionhelp"
@@ -20,25 +27,25 @@ func New(d deps.Deps) sandbox.SandBox {
 	sandbox.Config.Version = "v0.0.12"
 	sandbox.Config.ProjectName = "Agnos"
 
-	sandbox.Cli.Commands = []cli.CliCommand{
-		version.NewCommand(sandbox),
-		help.NewCommand(sandbox),
-		start.NewCommand(sandbox),
-		build.NewCommand(sandbox),
-		install.NewCommand(sandbox),
-		uninstall.NewCommand(sandbox),
-		list.NewCommand(sandbox),
-		extensionhelp.NewCommand(sandbox),
+	sandbox.Cli.Commands = []cliApi.CliCommand{
+		cmdVersion.NewCommand(&sandbox),
+		cmdHelp.NewCommand(&sandbox),
+		cmdStart.NewCommand(&sandbox),
+		cmdBuild.NewCommand(&sandbox),
+		cmdInstall.NewCommand(&sandbox),
+		cmdUninstall.NewCommand(&sandbox),
+		cmdList.NewCommand(&sandbox),
+		cmdExtensionhelp.NewCommand(&sandbox),
 	}
 
-	sandbox.Cli.CliMain = CliMainFactory(sandbox)
+	sandbox.Cli.CliMain = cliMain.CliMainFactory(&sandbox)
 
-	sandbox.Core.Start = start.StartFactory(sandbox)
-	sandbox.Core.Build = build.BuildFactory(sandbox)
-	sandbox.Core.Install = install.InstallFactory(sandbox)
-	sandbox.Core.Uninstall = uninstall.UninstallFactory(sandbox)
-	sandbox.Core.List = list.ListFactory(sandbox)
-	sandbox.Core.ExtensionHelp = extensionhelp.ExtensionHelpFactory(sandbox)
+	sandbox.Core.Start = start.StartFactory(&sandbox)
+	sandbox.Core.Build = build.BuildFactory(&sandbox)
+	sandbox.Core.Install = install.InstallFactory(&sandbox)
+	sandbox.Core.Uninstall = uninstall.UninstallFactory(&sandbox)
+	sandbox.Core.List = list.ListFactory(&sandbox)
+	sandbox.Core.ExtensionHelp = extensionhelp.ExtensionHelpFactory(&sandbox)
 
 	return sandbox
 }
