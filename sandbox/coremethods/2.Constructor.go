f455c71f4d961258571cb97ed34051a627b5856b
matesmoutinho <matesmoutinho01@gmail.com>
2026-08-30 20:16:59 -0300

    refactor: rename NewCoreMethods to New for improved constructor naming convention

--- sandbox/coremethods/2.Constructor.go
+++ sandbox/coremethods/2.Constructor.go
@@ -2,7 +2,7 @@ package coremethods
 
 import "github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/contracts/lib/sandbox"
 
-func NewCoreMethods(sandbox *sandbox.SandBox) *CoreMethods {
+func New(sandbox *sandbox.SandBox) *CoreMethods {
 	return &CoreMethods{
 		Sandbox: sandbox,
 	}
