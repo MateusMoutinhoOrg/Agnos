d612793ebb59f3c4829ef62b4cbbc276b8d68e8c
matesmoutinho <matesmoutinho01@gmail.com>
2026-08-30 22:37:55 -0300

    refactor: encapsulate sandbox dependencies by modularizing API contracts into dedicated packages

--- sandbox/deps/deps.go
+++ sandbox/deps/deps.go
@@ -1,4 +1,32 @@
 package deps
 
+import (
+	"time"
+
+	embeddeps "github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/deps/embeddeps"
+	iodeps "github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/deps/iodeps"
+	keepdeps "github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/deps/keepdeps"
+	requestdeps "github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/deps/requestdeps"
+	serializibles "github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/deps/serializebles"
+	verbdeps "github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/deps/verbdeps"
+)
+
 type Deps struct {
+	Now func() time.Time
+
+	Printf func(format string, a ...any) (n int, err error)
+	Error  func(format string, a ...any) (n int, err error)
+	Errorf func(format string, a ...any) error
+
+	VerbLib verbdeps.Lib
+
+	KeepLib keepdeps.Lib
+
+	EmbedDeps embeddeps.Lib
+
+	IoLib iodeps.Lib
+
+	NewRequest func(url string) requestdeps.Request
+
+	SerializeLib serializibles.Lib
 }
---
+++ sandbox/deps/serializebles/serializebles.go
@@ -0,0 +1,46 @@
+package serializibles
+
+type SerializibleObject struct {
+	IsInt    func() bool
+	IsString func() bool
+	IsFloat  func() bool
+	IsBool   func() bool
+	IsNull   func() bool
+	IsObject func() bool
+	IsArray  func() bool
+
+	GetInt    func() (int64, error)
+	GetFloat  func() (float64, error)
+	GetString func() (string, error)
+	GetBool   func() (bool, error)
+
+	GetObjectItem func(key string) (*SerializibleObject, error)
+	HasKey        func(key string) bool
+	GetKeys       func() ([]string, error)
+
+	GetArrayItem func(index int) *SerializibleObject
+	GetArraySize func() (int, error)
+
+	AddItemToObject      func(key string, item any) error
+	ReplaceItemInObject  func(key string, item any) error
+	DeleteItemFromObject func(key string) error
+
+	AddItemToArray      func(item any) error
+	DeleteItemFromArray func(index int) error
+}
+
+type Lib struct {
+	CreateString func(value string) *SerializibleObject
+	CreateInt    func(value int64) *SerializibleObject
+	CreateFloat  func(value float64) *SerializibleObject
+	CreateBool   func(value bool) *SerializibleObject
+	CreateNull   func() *SerializibleObject
+	CreateObject func() *SerializibleObject
+	CreateArray  func() *SerializibleObject
+
+	ParseJson func(data string) (*SerializibleObject, error)
+	ParseYaml func(data string) (*SerializibleObject, error)
+
+	SerializeToJson func(data *SerializibleObject) string
+	SerializeToYaml func(data *SerializibleObject) string
+}
