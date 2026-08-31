4b00e5950d4195d1db4d9685e1617df98342854d
matesmoutinho <matesmoutinho01@gmail.com>
2026-08-30 23:21:39 -0300

    refactor: migrate documentation and sandbox files to an archive directory and update main entry point

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,35 +1,19 @@
 package main
 
 import (
-	"os"
-	"path/filepath"
-
 	agnosadapter "github.com/MateusMoutinhoOrg/Agnos-Cli/adapters/standard"
+	agnosapi "github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/api"
+
 	agnoslib "github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox"
-)
-
-const (
-	dataDirName = ".agnos"
-
-	dataDirEnv = "AGNOS_DATA"
 )
 
 func main() {
 
-	deps := agnosadapter.New(dataPath())
+	deps := agnosadapter.New(".agnos")
 
 	l := agnoslib.New(&deps)
-	l.Build()
+	l.Build(agnosapi.BuildProps{
+		Path: ".",
+	})
 
-}
-
-func dataPath() string {
-	if override := os.Getenv(dataDirEnv); override != "" {
-		return override
-	}
-	home, err := os.UserHomeDir()
-	if err != nil {
-		return dataDirName
-	}
-	return filepath.Join(home, dataDirName)
 }
