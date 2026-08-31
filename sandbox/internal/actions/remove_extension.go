f76adf58e09054a15e6c2a79ae9752b811962f03
matesmoutinho <matesmoutinho01@gmail.com>
2026-08-31 00:23:05 -0300

    feat: implement extension management actions for install, remove, and list functionalities

---
+++ sandbox/internal/actions/remove_extension.go
@@ -0,0 +1,11 @@
+package actions
+
+import (
+	"github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/api"
+	"github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/deps"
+)
+
+func RemoveExtension(deps deps.Deps, props api.UninstallProps) error {
+	deps.Printf("remove extension started with item %s \n", props.Item)
+	return nil
+}
--- sandbox/new.go
+++ sandbox/new.go
@@ -13,5 +13,14 @@ func New(deps *deps.Deps) *api.Sandbox {
 	self.Build = func(props api.BuildProps) error {
 		return actions.Build(*deps, props)
 	}
+	self.InstallExtension = func(props api.InstallProps) error {
+		return actions.InstallExtension(*deps, props)
+	}
+	self.RemoveExtension = func(props api.UninstallProps) error {
+		return actions.RemoveExtension(*deps, props)
+	}
+	self.ListAvaliableExtensions = func(props api.ListAvailableExtensionsProps) ([]string, error) {
+		return actions.ListAvailableExtensions(*deps, props)
+	}
 	return &self
 }
