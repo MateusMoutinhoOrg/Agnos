0113978f29d8f3e3826af6d26d27759e8c1ffb35
matesmoutinho <matesmoutinho01@gmail.com>
2026-09-01 04:13:38 -0300

    feat: inject sandbox binds into the generated new.go template automatically

--- assets/sandbox/new.go
+++ assets/sandbox/new.go
@@ -2,12 +2,15 @@ package sandbox
 
 import (
 	api "{{.Module}}/sandbox/api"{{if .HasDeps}}
-	deps "{{.Module}}/sandbox/deps"{{end}}
+	deps "{{.Module}}/sandbox/deps"{{end}}{{if .Binds}}
+
+	binds "{{.Module}}/sandbox/binds"{{end}}
 )
 
 {{if .HasDeps}}func New(deps *deps.Deps) *api.Sandbox {
 {{else}}func New() *api.Sandbox {
 {{end}}	self := api.Sandbox{}
-
+{{range .Binds}}	binds.{{.}}({{if $.HasDeps}}deps, {{end}}&self)
+{{end}}
 	return &self
 }
--- sandbox/internal/actions/build/render_new.go
+++ sandbox/internal/actions/build/render_new.go
@@ -1,6 +1,8 @@
 package build
 
 import (
+	"strings"
+
 	"github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/deps"
 	"github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/internal/smartio"
 	"github.com/MateusMoutinhoOrg/Agnos-Cli/sandbox/internal/utils"
@@ -8,9 +10,30 @@ import (
 
 func Render_sandbox_new_go(deps *deps.Deps, io *smartio.SmartIO, module string) error {
 
+	files := io.ListFiles("sandbox/binds")
+
+	var binds []string
+	for _, file := range files {
+		parts := strings.Split(file, "/")
+		name := parts[len(parts)-1]
+
+		if !strings.HasSuffix(name, ".go") {
+			continue
+		}
+
+		baseName := strings.TrimSuffix(name, ".go")
+		if len(baseName) == 0 {
+			continue
+		}
+
+		title := strings.ToUpper(baseName[:1]) + baseName[1:]
+		binds = append(binds, title+"Bind")
+	}
+
 	vars := map[string]interface{}{
 		"Module":  module,
 		"HasDeps": smartio.IsDir(deps, io, "sandbox/deps"),
+		"Binds":   binds,
 	}
 	err := utils.RenderTemplateToDest(deps, io, "sandbox/new.go", vars, "sandbox/new.go")
 	if err != nil {
